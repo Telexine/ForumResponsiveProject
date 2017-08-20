4562c2b3c3aa5f93a0d095f065548cfc31affb98
Lexine Langford <tanuki360@hotmail.com>
2017-08-20 16:14:33 +0700

    add comment testUnit func.php

--- test.php
+++ test.php
@@ -13,12 +13,22 @@ require_once('func.php');
 <body>
 
 <?php
- // UpdateUser($ID,$NAME,$PW,$Avartar){
- //echo UpdateUser('test','c'," ","d");
+
+
+
+
+
+/* // Test  UpdateUser
+ echo UpdateUser('test','c'," ","d");
+*/
+
+
+
+ /* // Test login
  login('test','123232323');
  print_r( $_SESSION['user_info']);
-
  echo $_SESSION['user_info']['User_ID']
+ */
 ?>
 </body>
 <html>
