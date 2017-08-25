4b7f99fc24c69c049c18de13673870ff51b2aee1
Lexine Langford <tanuki360@hotmail.com>
2017-08-25 19:55:12 +0700

    UI create function append post

--- index.php
+++ index.php
@@ -65,23 +65,22 @@ require_once('resources/PHP/func.php');
     <header class="mdl-layout__header " style="background-color:#E0E0E0;padding-top:10px;">
         <h5>      HOT  POST</h1>
         </header>
-    <div id="hotpost">
+    <div id="hotpost" style="width:87%;margin:auto">
     
+    <div class="demo-card-square mdl-card mdl-shadow--2dp floatLeft">
+			<div class="mdl-card__title mdl-card--expand">
+				<h2 class="mdl-card__title-text">ประกาศจ้าา    <p> tae</p> </h2> 
+			</div>
 
-
-
-    <div class="demo-card-square mdl-card mdl-shadow--2dp">
-  <div class="mdl-card__title mdl-card--expand">
-    <h2 class="mdl-card__title-text">ประกาศจ้าา    <p> tae</p> </h2> 
-  </div>
-  <div class="mdl-card__supporting-text">
-    งานทำยังไมเสร็จ
-  </div>
-  <div class="mdl-card__actions mdl-card--border">
-    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-      Go to Post
-    </a>
-  </div>
+		<div class="mdl-card__supporting-text">
+			งานทำยังไมเสร็จ
+		</div>
+		<div class="mdl-card__actions mdl-card--border">
+			<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+				Go to Post
+			</a>
+			<div class="rate"></div>
+		</div>
 </div>
 
 
@@ -99,6 +98,9 @@ require_once('resources/PHP/func.php');
     </div>
 </div>
   </main>
+
+
+
 </div>
 
 
@@ -106,8 +108,69 @@ require_once('resources/PHP/func.php');
 
 
 
-
-
-
-
 </body>
+<!-- TAE  -->
+<script>
+
+	 // Add a message to the messages element.
+	 function appendPost(PostID,imageURL,Title,Nickname,content) {
+
+		 imageURL = "";
+		  Title="title";Nickname="nickname";content="content";
+	   //GET WHERE to append post
+	   const cardElement = document.querySelector('#hotpost');
+
+		//create element 
+	   const cardDivElement = document.createElement('div');
+	   const titleElement = document.createElement('div');
+		 const title_text = document.createElement('h2');
+	   const contentElement = document.createElement('div');
+	   const buttonBox = document.createElement('div');
+		 
+	   const buttonElement =document.createElement('div');
+		 const rateElement = document.createElement('div');
+
+	   // add class 
+
+	   cardDivElement.className = " demo-card-square mdl-card mdl-shadow--2dp floatLeft";
+		 titleElement.className = "mdl-card__title mdl-card--expand";
+		 title_text.className = "mdl-card__title-text";
+		 contentElement.className = "mdl-card__supporting-text";
+		 buttonBox.className = "mdl-card__actions mdl-card--border";
+		 rateElement.className="rate";
+		 buttonElement.className = "mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect";
+
+	   //int metadata 
+		 title_text.textContent = Title+" "+Nickname+" ";
+		 title_text.style.background = "url('"+imageURL+"') center / cover";
+		 contentElement.textContent = content;
+		 buttonElement.innerHTML= "GO TO POST";
+  
+	   //append Messgage
+
+	   cardDivElement.appendChild(titleElement);
+	   titleElement.appendChild(title_text);
+		 cardDivElement.appendChild(contentElement);
+	
+	   
+	   buttonBox.appendChild(buttonElement);
+	   buttonBox.appendChild(rateElement);
+
+		 cardDivElement.appendChild(buttonBox);
+
+	   cardElement.appendChild(cardDivElement);
+	 }
+
+
+
+
+</script>
+
+<style>
+.floatLeft{
+	float: left;
+
+}
+
+
+</style>
